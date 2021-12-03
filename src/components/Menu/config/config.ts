2545d82e103fff97eb9bcac8079a0aa89f3afe99
zak <topdeveloper530@gmail.com>
2021-12-03 15:51:51 +1000

    cahu_swap

---
+++ src/components/Menu/config/config.ts
@@ -0,0 +1,94 @@
+import { MenuItemsType} from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+
+export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
+
+const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
+  {
+    label: t('Exchange'),
+    href: '/swap',
+    showItemsOnMobile: false,
+    items: []
+  },
+  {
+    label: t('Liquidity'),
+    href: '/liquidity',
+    items: []
+  }
+  // {
+  //   label: t('Win'),
+  //   href: '/prediction',
+  //   icon: 'Trophy',
+  //   items: [
+  //     {
+  //       label: t('Prediction (BETA)'),
+  //       href: '/prediction',
+  //     },
+  //     {
+  //       label: t('Lottery'),
+  //       href: '/lottery',
+  //     },
+  //   ],
+  //  }
+  // {
+  //   label: t('NFT'),
+  //   href: `${nftsBaseUrl}`,
+  //   icon: 'Nft',
+  //   items: [
+  //     {
+  //       label: t('Overview'),
+  //       href: `${nftsBaseUrl}`,
+  //       status: {
+  //         text: t('Live'),
+  //         color: 'failure',
+  //       },
+  //     },
+  //     {
+  //       label: t('Collections'),
+  //       href: `${nftsBaseUrl}/collections`,
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: '',
+  //   href: '/info',
+  //   icon: 'More',
+  //   hideSubNav: true,
+  //   items: [
+  //     {
+  //       label: t('Info'),
+  //       href: '/info',
+  //     },
+  //     {
+  //       label: t('IFO'),
+  //       href: '/ifo',
+  //     },
+  //     {
+  //       label: t('Voting'),
+  //       href: '/voting',
+  //     },
+  //     {
+  //       type: DropdownMenuItemType.DIVIDER,
+  //     },
+  //     {
+  //       label: t('Leaderboard'),
+  //       href: '/teams',
+  //     },
+  //     {
+  //       type: DropdownMenuItemType.DIVIDER,
+  //     },
+  //     {
+  //       label: t('Blog'),
+  //       href: 'https://pancakeswap.medium.com',
+  //       type: DropdownMenuItemType.EXTERNAL_LINK,
+  //     },
+  //     {
+  //       label: t('Docs'),
+  //       href: 'https://docs.pancakeswap.finance',
+  //       type: DropdownMenuItemType.EXTERNAL_LINK,
+  //     },
+  //   ],
+  // },
+]
+
+export default config
---
+++ src/views/Nft/market/Collection/Items/CollectionNfts.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import { Grid } from '@pancakeswap/uikit'
+import { getAddress } from '@ethersproject/address'
+import orderBy from 'lodash/orderBy'
+import { useAppDispatch } from 'state'
+import { useNftsFromCollection } from 'state/nftMarket/hooks'
+import { Collection } from 'state/nftMarket/types'
+import { fetchNftsFromCollections } from 'state/nftMarket/reducer'
+import GridPlaceholder from '../../components/GridPlaceholder'
+import { CollectibleLinkCard } from '../../components/CollectibleCard'
+import { pancakeBunniesAddress } from '../../constants'
+import useAllPancakeBunnyNfts from '../../hooks/useAllPancakeBunnyNfts'
+
+interface CollectionNftsProps {
+  collection: Collection
+  sortBy?: string
+}
+
+const CollectionNfts: React.FC<CollectionNftsProps> = ({ collection, sortBy = 'updatedAt' }) => {
+  const { address } = collection
+  const checksummedAddress = getAddress(address)
+  const dispatch = useAppDispatch()
+
+  const isPBCollection = address === pancakeBunniesAddress
+
+  useEffect(() => {
+    dispatch(fetchNftsFromCollections(checksummedAddress))
+  }, [checksummedAddress, dispatch])
+
+  const nfts = useNftsFromCollection(checksummedAddress)
+  const allPancakeBunnyNfts = useAllPancakeBunnyNfts(address)
+
+  const currentNfts = isPBCollection ? allPancakeBunnyNfts : nfts?.filter((nft) => nft.marketData.isTradable)
+
+  if (!currentNfts) {
+    return <GridPlaceholder />
+  }
+
+  const nftsToShow = orderBy(
+    currentNfts,
+    (nft) => (isPBCollection ? nft.meta[sortBy] : Number(nft.marketData[sortBy])),
+    [sortBy === 'currentAskPrice' ? 'asc' : 'desc'],
+  )
+
+  return (
+    <Grid
+      gridGap="16px"
+      gridTemplateColumns={['1fr', null, 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
+      alignItems="start"
+    >
+      {nftsToShow.map((nft) => {
+        return <CollectibleLinkCard key={`${nft.tokenId}-${nft.collectionName}`} nft={nft} />
+      })}
+    </Grid>
+  )
+}
+
+export default CollectionNfts
